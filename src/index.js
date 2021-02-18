d04fe5c90f0468525891fbe325726b14dfc7c5e3
Randy <rgoldsmith@nieto.com>
2021-02-18 14:40:52 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import CommentDetail from './CommentDetail';
+
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail author="Sam" date="Today at 11:00AM" text="Great Job"/>
+      <CommentDetail author="Joey" date="Today at 4:00PM" text="Yeah I had fun!"/>
+      <CommentDetail author="Sarah" date="Today at 6:00PM" text="Me too!"/>
+    </div>
+  );
+};
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
